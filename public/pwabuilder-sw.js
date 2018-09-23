bf49bd8ecfabada8a21462686bec0e0e56b77fa0
Aleksandra Mazunina <aleksandra.mazunina@oxagile.com>
2018-09-23 15:07:38 +0200

    start

---
+++ public/pwabuilder-sw.js
@@ -0,0 +1,4 @@
+//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+
+//Install stage sets up the offline page in the cache and opens a new cache
+
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Oxa Social</h1>
+        </header>
+        <p className="App-intro">
+          <input type="text"/>
+          <button>Save</button>
+        </p>
+        <div className="bill-storage">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
